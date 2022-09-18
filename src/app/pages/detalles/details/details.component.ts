3c2d7878709dabd722acdb9171efdf23dcd9d6ac
bonillasan <sbonilla0197@gmail.com>
2022-09-18 08:29:53 -0500

    final

---
+++ src/app/pages/detalles/details/details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  usuario:any=[]
+  constructor(private UsuarioSvc:UsuariosService) { }
+
+  ngOnInit(): void {
+    this.usuario = new Object(JSON.parse(this.getNombre()));
+  }
+
+  public getNombre():any{
+    return localStorage.getItem('user');
+  }
+
+}
